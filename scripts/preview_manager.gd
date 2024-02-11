2172354748aea2db79ead8b845512b906de13db5
Eduard Hopfer <edo15115@gmail.com>
2024-02-11 10:23:38 +0100

    cleanup the hierarchy

--- scripts/preview_manager.gd
+++ scripts/preview_manager.gd
@@ -1,9 +1,6 @@
 class_name PreviewManager
 extends Node2D
 
-signal preview_opened
-signal preview_closed
-
 var node_holding_preview: ClickableArea = null
 	
 func _input(event):		
@@ -14,7 +11,6 @@ func try_lock_preview(opened_node: ClickableArea):
 	print("try lock")
 	if node_holding_preview == null:
 		node_holding_preview = opened_node
-		preview_opened.emit()
 		return true
 		
 	return false
