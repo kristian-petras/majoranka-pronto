849615d58045ba8a87fec8dbbb1c5b814cb77b6a
tino <kpetras@tutanota.com>
2024-02-11 20:28:39 +0100

    fix time

--- scenes/clock_ticker.gd
+++ scenes/clock_ticker.gd
@@ -4,13 +4,21 @@ extends Sprite2D
 
 var _current_hour = 0
 
+var step = 3.75
+var current_rotation = 180
+
 # 0-11 throught the game
 signal current_hour(hour)
 
 func _on_tick(counter):
+	current_rotation += step
+	rotation_degrees = current_rotation
 	if counter % hour_tick_count == 0:
+		print("rotation and hour")
+		print(current_rotation)
+		print(_current_hour)
 		current_hour.emit(_current_hour)
 		_current_hour += 1
 
 func _on_current_hour(hour):
-	rotation_degrees = hour * 30
+	pass
